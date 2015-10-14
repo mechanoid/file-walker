c6620f4279d7add1ea6c7161791042fb15e08959
Falk Hoppe <falkhoppe81@gmail.com>
2015-10-14 16:07:54 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# file walker
+
+the file walker lib is a simple lib (yet in draft status) to crawl a folder and its sub folders.
+
+This project is merely a promise testing project, so don't expect too much from it.
---
+++ file-walker.js
@@ -0,0 +1,68 @@
+var fs = require('fs')
+    , path = require('path')
+    , Q = require('q')
+    , lo = require('lodash');
+
+
+
+module.exports = {
+  resolveFile(dir, file) {
+    file = path.resolve(dir, file);
+    var d = Q.defer();
+
+    fs.stat(file, (err, stat) => {
+      if (err) {
+        d.reject(err);
+      } else {
+        if (stat && stat.isDirectory()) {
+          d.resolve(this.run(file));
+        } else {
+          d.resolve(file);
+        }
+      }
+    });
+
+    return d.promise;
+  }
+
+  , readDir(dir) {
+    var d = Q.defer();
+
+    fs.readdir(dir, function(err, files) {
+      if (err) {
+        d.reject(err);
+      } else {
+        d.resolve(files);
+      }
+    });
+    return d.promise;
+  }
+
+  , run(dir) {
+    var d = Q.defer();
+
+    this.readDir(dir)
+    .then((files) => {
+      var promises = files.map((file) => { return this.resolveFile(dir, file) });
+
+      Q.all(promises)
+      .then(function(files) {
+        d.resolve(lo.flatten(files));
+      });
+    })
+    .catch(function(error) {
+      d.reject(error);
+    });
+
+    return d.promise;
+  }
+
+  , walk(dir) {
+    var r = this.run(dir).then(function(files) {
+      console.log(files);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
+};
